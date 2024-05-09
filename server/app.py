c406c2364b77ac34800b58e6ec2b3124c1532f2a
minhphamcompcsc <minh.phamcompcsc@gmail.com>
2024-05-09 10:35:15 +0700

    something

--- server/app.py
+++ server/app.py
@@ -183,7 +183,6 @@ def handle_send_message(data):
     emit('receive_message', data, broadcast=True)
 
 
-
 @app.route('/api/changepassword/<userId>', methods=['POST'])
 def changepassword(userId : str):
     # print(userId)
